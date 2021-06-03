3fd48716aa00f48cce1fb73c4c7122fd9e64c498
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2021-06-03 15:20:02 +0700

    Update stack-list.cpp
    update 1

--- stack-list.cpp
+++ stack-list.cpp
@@ -1,15 +1,15 @@
 /*Tugas Kelompok Stack List
 Nama-anggota:
-1. 
-2.
-3.
+1. Bagus Tito 
+2. Muhammad Febrian H   
+3. Nur Mita Utami
 
 Link Github: .....
 */
 #include <bits/stdc++.h>
-#define MAX_STACK_SIZE 100
 
 using namespace std;
+
 class Lingkaran{
 public:
     double radius;
@@ -35,25 +35,65 @@ public:
 class ListStack{
 public:
     Lingkaran *top;
-    ArrayStack(){
+    ListStack(){
         top=NULL;
     }
     bool isEmpty(){
-        //anggota 1
+        if(top == NULL){
+            return 1;}
+        else{
+            return 0;
+            }
     }
-    bool isFull(){
-        //anggota 1
-    }
+
     void push(Lingkaran *item){
-        //anggota 2
+        item->next = NULL;
+        if (isEmpty()){
+            item = top;
+        } else{
+            item->next = top;
+            top = item;
+        }
     }
+
     void push(double radius){
-        //anggota 2
+        Lingkaran *item;
+        item = new Lingkaran(radius);
+        item->next = NULL;
+        if (isEmpty()){
+           top = item;
+        } else {
+            item -> next=top;
+            top = item;
+        }
     }
+
     Lingkaran *pop(){
-        //anggota 3
+        Lingkaran *del;
+        if (isEmpty()) {
+            cout<<"Stack Kosong"<<endl;
+            return NULL;
+        }else{
+            del = top;
+            top = top->next;
+            return del;
+            delete del;
+            return NULL;
+        }
     }
 };
+
 int main(){
-    
+    ListStack stack;
+    Lingkaran l1(10);
+    stack.push(5.5);
+    stack.push(15);
+    stack.push(20);
+    stack.push(35);
+    cout<<stack.pop()->radius<<endl;
+    cout<<stack.pop()->radius<<endl;
+    cout<<stack.pop()->radius<<endl;
+    cout<<stack.pop()->radius<<endl;
+    cout<<stack.pop()->radius<<endl;
+    return 0;
 }
